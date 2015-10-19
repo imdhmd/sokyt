a0eba18726b919e91b1975f77eb0d054fdbf11dc
Imdad Ahmed <imdhmd@gmail.com>
2015-10-19 17:45:58 +0530

    Server need not wait for handlers to finish before quit
    - Server socket can be closed
    - Since socket streams from other handlers are still open, that
    - communication can continue

--- src/main/java/com/imdhmd/p2p/server/Server.java
+++ src/main/java/com/imdhmd/p2p/server/Server.java
@@ -3,19 +3,16 @@ package com.imdhmd.p2p.server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.HashSet;
 
 import static com.imdhmd.p2p.Main.log;
 
 public class Server extends Thread {
     private ServerSocket socket;
     private Shared shared;
-    private HashSet<RequestHandler> handlers;
 
     public Server() throws IOException {
         socket = new ServerSocket(8812);
         shared = new Shared();
-        handlers = new HashSet<>();
     }
 
     public void run() {
@@ -24,44 +21,18 @@ public class Server extends Thread {
         try {
             while (true) {
                 Socket requestSocket = accept();
-                if (shared.quit()) {
-                    waitForRemainingHandlers();
+                if (shared.quit())
                     break;
-                }
 
                 log("S New request received");
-                newRequestHandler(requestSocket).start();
-                freeUpHandlers();
+                new RequestHandler(requestSocket, shared).start();
             }
-        } catch (IOException | InterruptedException e) {
+        } catch (IOException e) {
             throw new RuntimeException(e);
         } finally {
             close();
             log("S Server done");
         }
-    }
-
-    private void waitForRemainingHandlers() throws InterruptedException {
-        for (RequestHandler handler : handlers) {
-            if (handler.isAlive())
-                handler.join();
-        }
-    }
-
-    private RequestHandler newRequestHandler(Socket requestSocket) throws IOException {
-        RequestHandler handler = new RequestHandler(requestSocket, shared);
-        handlers.add(handler);
-        return handler;
-    }
-
-    private void freeUpHandlers() {
-        HashSet<RequestHandler> aliveHandlers = new HashSet<>();
-        for (RequestHandler handler : handlers) {
-            if (handler.isAlive())
-                aliveHandlers.add(handler);
-        }
-        handlers.clear();
-        handlers.addAll(aliveHandlers);
     }
 
     private void close() {
